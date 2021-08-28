405ae474d00976301311ee6e59b783f26436493d
galvao <fgalvao90@yahoo.com>
2021-08-28 18:33:55 +0100

    add task controller, model and migration

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Task extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'tasks';
+    protected $guarded = ['id'];
+
+    /**
+     * RELATIONSHIPS
+     */
+
+    public function user() {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+}
---
+++ database/migrations/2021_08_28_141919_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description',2500)->nullable();
+            $table->integer('status')->default(0);
+            $table->foreignId('user_id')->nullable()->references('id')->on('users')->cascadeOnDelete();
+            $table->dateTime('done_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}

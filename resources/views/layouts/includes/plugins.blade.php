3967bbfecd7d767f081442452fd6d2300d4355b3
galvao <fgalvao90@yahoo.com>
2021-08-29 01:32:19 +0100

    update views

--- resources/views/layouts/includes/plugins.blade.php
+++ resources/views/layouts/includes/plugins.blade.php
@@ -1,14 +1,8 @@
 <!-- jQuery -->
 <script src="/plugins/jquery/jquery.min.js"></script>
+<script src="/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/js/adminlte.min.js"></script>
-
-<!-- Datatables -->
-<script src="/plugins/datatables/jquery.dataTables.js"></script>
-<script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
-<script src ="/plugins/datatables-responsive/js/dataTables.responsive.js"></script>
-<script src ="/plugins/datatables-responsive/js/responsive.bootstrap4.js"></script>
-
 

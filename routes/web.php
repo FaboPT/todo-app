b03b4c5f860531bc4e67b2b4e6a6fe5730e2d108
Fábio Galvão <fabiogalvao@MacBook-Pro.Home>
2022-01-11 09:22:43 +0000

    Clean code

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
+\Illuminate\Support\Facades\Auth::routes();
 
 Route::get('/', function () {
     return redirect()->route('task.index');
